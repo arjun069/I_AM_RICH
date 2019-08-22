9c00fc960eff8dae32287f516e7bf7f56eb27c7d
Arjun <arjunnair2k18@gmail.com>
2019-08-22 23:29:33 +0530

    First Changes

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey[900],
+        title: Text('                             I am RICH'),
+      )
+        backgroundColor: Colors.blueGrey,
+      body: Center(
+        child: Image(image: AssetImage('images/diamond.png')),
+    ),
+    ),
+  ));
+}
